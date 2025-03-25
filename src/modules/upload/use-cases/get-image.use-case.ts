679e527fab43cf36115f97a7971e671fa9bca772
Juan Versolato Lopes <juan.versolato@outlook.com>
2025-03-25 18:41:10 -0300

    chore: run prettier.

--- src/modules/upload/use-cases/get-image.use-case.ts
+++ src/modules/upload/use-cases/get-image.use-case.ts
@@ -2,11 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class GetImageUseCase {
-  public async execute(): Promise<any> {
-    try {
-      return { image: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_AWS_REGION}.amazonaws.com/logo` };
-    } catch (error) {
-      throw error;
-    }
+  public execute(): any {
+    return { image: `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.S3_AWS_REGION}.amazonaws.com/logo` };
   }
 }
